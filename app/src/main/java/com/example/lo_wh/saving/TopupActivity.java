a89e4dcbce7631923dd5816a6a8436aca457f693
abc <abc@abc.com>
2018-10-06 14:03:58 +0800

    Top Up Draft

--- app/src/main/java/com/example/lo_wh/saving/TopupActivity.java
+++ app/src/main/java/com/example/lo_wh/saving/TopupActivity.java
@@ -1,7 +1,9 @@
 package com.example.lo_wh.saving;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class TopupActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class TopupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topup);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.topup_toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+
     }
 }
