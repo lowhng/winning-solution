5d1ea8b10f1f71c356a810a94b68ec9bbd9a7b9b
lowhng <khcy4lwo@nottingham.edu.my>
2018-10-06 17:38:34 +0800

    Started game screen (hardcoded)

--- app/src/main/java/com/example/lo_wh/saving/GameActivity.java
+++ app/src/main/java/com/example/lo_wh/saving/GameActivity.java
@@ -2,12 +2,25 @@ package com.example.lo_wh.saving;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 
 public class GameActivity extends AppCompatActivity {
+
+    ImageView mImage_in;
+    ImageView mImage_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        mImage_in = findViewById(R.id.inside_imageview);
+        mImage_out = findViewById(R.id.outside_imageview);
+
+        mImage_in.setAdjustViewBounds(true);
+        mImage_in.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+
+
     }
 }
