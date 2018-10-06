a6eb7806a60f1416c00f380745e81c905d42f77e
abc <abc@abc.com>
2018-10-07 02:27:15 +0800

    Added persistence via SharedPreferences with JSON Fixed basic interactions Added checking for insufficient board space

--- app/src/main/java/com/example/lo_wh/saving/GameActivity.java
+++ app/src/main/java/com/example/lo_wh/saving/GameActivity.java
@@ -1,46 +1,76 @@
 package com.example.lo_wh.saving;
 
 import android.content.ClipData;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.DragEvent;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 
 public class GameActivity extends AppCompatActivity {
 
+    final String imagePrefix = "white_level";
+    final String viewPrefix = "outside_imageview";
     ImageView mImage_in;
-    ImageView mImage_out;
-    final String imagePreset = "yellow_level";
-    HashMap<String,Integer> hashMap = new HashMap<String,Integer>()
-    {{
-        put("1",0);
-        put("2",0);
-        put("3",0);
-        put("4",0);
-        put("5",0);
-        put("6",0);
-        put("7",0);
-        put("8",0);
-        put("9",0);
-    }};
+    HashMap<String,Integer> hashMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        hashMap = new HashMap<>();
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("gameHashMap", Context.MODE_PRIVATE);
+        try {
+            if (sharedPref != null) {
+                System.out.println("Shared Preferences Found!");
+                JSONObject jsonHashMap = new JSONObject(sharedPref.getString("hashMap", (new JSONObject()).toString()));
+                printJsonHashMap(jsonHashMap);
+                Iterator<String> keyIterator = jsonHashMap.keys();
+                while(keyIterator.hasNext()){
+                    String currentKey = keyIterator.next();
+                    hashMap.put(currentKey,jsonHashMap.getInt(currentKey));
+                }
+            } else {
+                System.out.println("Shared Preferences Not Found!");
+                hashMap = null;
+            }
+        }catch(JSONException e){
+            e.printStackTrace();
+            hashMap = null;
+        }catch(Exception e){
+            e.printStackTrace();
+            hashMap = null;
+        }
+        if (hashMap == null){
+            initHashMap();
+        }else{
+            initialRender();
+        }
+
         mImage_in = findViewById(R.id.inside_imageview);
         mImage_in.setAdjustViewBounds(true);
         mImage_in.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
         findViewById(R.id.btn_buy).setOnClickListener(new onClickListener());
 
-        //Drop
+        //Drop Listener
         findViewById(R.id.outside_imageview1).setOnDragListener(new dragEventListener());
         findViewById(R.id.outside_imageview2).setOnDragListener(new dragEventListener());
         findViewById(R.id.outside_imageview3).setOnDragListener(new dragEventListener());
@@ -52,6 +82,42 @@ public class GameActivity extends AppCompatActivity {
         findViewById(R.id.outside_imageview9).setOnDragListener(new dragEventListener());
     }
 
+    private void initHashMap(){
+        //Initialize HashMap for first time players
+        hashMap = new HashMap<String,Integer>()
+        {{
+            put("1",0);
+            put("2",0);
+            put("3",0);
+            put("4",0);
+            put("5",0);
+            put("6",0);
+            put("7",0);
+            put("8",0);
+            put("9",0);
+        }};
+    }
+
+    private void initialRender(){
+        Log.d("Map Render","Running initial render...");
+        //First render of saved game
+        Iterator hashMapIterator = hashMap.entrySet().iterator();
+        while(hashMapIterator.hasNext()){
+            Map.Entry<String, Integer> keyValuePair = (Map.Entry<String, Integer>)hashMapIterator.next();
+
+            String curImageViewIdString = viewPrefix + keyValuePair.getKey();
+            int curImageViewId = getResources().getIdentifier(curImageViewIdString,"id", GameActivity.this.getPackageName());
+            ImageView curImageView = (ImageView)findViewById(curImageViewId);
+
+            if(keyValuePair.getValue() != 0){
+                String curImageIdString = imagePrefix + keyValuePair.getValue().toString();
+                int curImageId = getResources().getIdentifier(curImageIdString,"drawable", GameActivity.this.getPackageName());
+                curImageView.setImageResource(curImageId);
+                curImageView.setOnTouchListener(new dragTouchListener());
+            }
+        }
+    }
+
     private final class onClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v){
@@ -59,8 +125,8 @@ public class GameActivity extends AppCompatActivity {
             Integer i = 1;
             for( ; i <= hashMap.size() && !placed ; i++){
                 String intConv = i.toString();
-                System.out.println("Iterating " + intConv);
-                System.out.println("Value = " + hashMap.get(intConv).toString());
+                Log.d("NewBuildingPlacement","Iterating " + intConv);
+                Log.d("NewBuildingPlacement","Value = " + hashMap.get(intConv).toString());
                 if (hashMap.get(intConv).intValue() == 0){
                     placed = true;
                     Integer tempVal = hashMap.get(intConv);
@@ -68,8 +134,8 @@ public class GameActivity extends AppCompatActivity {
                     hashMap.put(intConv,tempVal);
 
                     String newBuildingViewName = "outside_imageview" + intConv;
+                    Log.d("NewBuildingPlacement", "Found empty lot " + newBuildingViewName);
                     int id = getResources().getIdentifier(newBuildingViewName,"id", GameActivity.this.getPackageName());
-                    System.out.println(id);
 
                     ImageView newBuildingView = findViewById(id);
                     //Set Level 1
@@ -77,10 +143,18 @@ public class GameActivity extends AppCompatActivity {
 
                     //Set Draggable
                     newBuildingView.setOnTouchListener(new dragTouchListener());
+
+                    //Save Results to Shared Preferences
+                    saveResults();
                 }
             }
             if(!placed){
                 //No space
+                Toast noSpaceToast = Toast.makeText(getApplicationContext(),
+                        "No space remaining for new buildings, please merge existing buildings to continue",
+                        Toast.LENGTH_LONG);
+                noSpaceToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                noSpaceToast.show();
             }
         }
     }
@@ -103,13 +177,7 @@ public class GameActivity extends AppCompatActivity {
 
     protected class dragEventListener implements View.OnDragListener {
 
-        final String imagePrefix = "white_level";
-        final String viewPrefix = "outside_imageview";
-
         public boolean onDrag(View v, DragEvent event){
-
-            int x_cord;
-            int y_cord;
 
             final int action = event.getAction();
 
@@ -124,8 +192,8 @@ public class GameActivity extends AppCompatActivity {
                     String targetViewName = getResources().getResourceName(targetView.getId());
                     String targetViewNumber = targetViewName.substring(targetViewName.indexOf(viewPrefix) + viewPrefix.length());
 
-                    System.out.println("Moving view" + movedViewNumber);
-                    System.out.println("Dropped on view" + targetViewNumber);
+                    Log.d("DragEvent","Moving " + viewPrefix + movedViewNumber);
+                    Log.d("DragEvent","Dropped on " + viewPrefix + targetViewNumber);
 
                     //Add target level
                     int targetLevel = hashMap.get(targetViewNumber);
@@ -136,6 +204,9 @@ public class GameActivity extends AppCompatActivity {
                     int targetImageId = getResources().getIdentifier(imagePrefix + targetLevel,"drawable", GameActivity.this.getPackageName());
                     targetView.setImageResource(targetImageId);
 
+                    //Set listener (in case of level 0 -> 1
+                    targetView.setOnTouchListener(new dragTouchListener());
+
                     //Reset moved level
                     hashMap.put(movedViewNumber,0);
 
@@ -143,9 +214,39 @@ public class GameActivity extends AppCompatActivity {
                     movedView.setImageResource(android.R.color.transparent);
 
                     //Remove drag listener
-                    movedView.setOnClickListener(null);
+                    movedView.setOnTouchListener(null);
+
+                    //Save to Shared Preferences
+                    saveResults();
             }
             return true;
         }
+    }
+
+    private void saveResults(){
+        Log.d("SaveHashMap","Attempting to save results!");
+        //Save results to Shared Preferences
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("gameHashMap", Context.MODE_PRIVATE);
+        JSONObject jsonHashMap = new JSONObject(hashMap);
+        SharedPreferences.Editor sharedPrefEditor = sharedPref.edit();
+        sharedPrefEditor.remove("hashMap").commit();
+        sharedPrefEditor.putString("hashMap", jsonHashMap.toString());
+        sharedPrefEditor.commit();
+
+        //DEBUG: Check results
+        SharedPreferences sharedPrefCheck = getApplicationContext().getSharedPreferences("gameHashMap", Context.MODE_PRIVATE);
+        try {
+            JSONObject jsonHashMapCheck = new JSONObject(sharedPrefCheck.getString("hashMap", (new JSONObject()).toString()));
+            printJsonHashMap(jsonHashMapCheck);
+        }catch (JSONException e){
+            e.printStackTrace();
+            Log.d("SaveHashMap","JSON Check Failed");
+        }
+    }
+
+    private void printJsonHashMap(JSONObject jsonHashMap){
+        Log.d("JSONHashMap","===========Check============");
+        Log.d("JSONHashMap", jsonHashMap.toString());
+        Log.d("JSONHashMap","==========End=Check=========");
     }
 }
